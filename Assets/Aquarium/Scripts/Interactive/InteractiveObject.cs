b3c4dfa9fd23bde49bee66a53c706ba3e57dfa50
DESKTOP-O44A0LA\TIGERBROT <mulddung2000@gmail.com>
2025-12-30 07:01:29 +0900

    1. 리팩토링

--- Assets/Aquarium/Scripts/Interactive/InteractiveObject.cs
+++ Assets/Aquarium/Scripts/Interactive/InteractiveObject.cs
@@ -80,7 +80,7 @@ namespace Aquarium
 #endif
     }
 }*/
-
+/*
 using UnityEngine;
 
 namespace Aquarium
@@ -178,5 +178,86 @@ namespace Aquarium
         }
 
         #endregion
+    }
+}*/
+using UnityEngine;
+
+namespace Aquarium
+{
+    public class InteractiveObject : MonoBehaviour
+    {
+        #region Variables
+        [Header("Interaction Info")]
+        [SerializeField] private string interactionName;
+        [SerializeField] private string goalText;
+
+        [Header("Dialogue")]
+        [SerializeField] private string[] dialogueLines;
+
+        [Header("Movement")]
+        [SerializeField] private Transform interactionPoint;
+        [SerializeField] private float interactionRadius = 1.2f;
+
+        [Header("Next Interaction")]
+        [SerializeField] private GameObject nextInteraction;
+        #endregion
+
+        #region Properties
+        public Transform InteractionPoint => interactionPoint;
+        public float InteractionRadius => interactionRadius;
+        #endregion
+
+        #region Unity Events
+        private void OnEnable()
+        {
+            UIManager.Instance.SetGoal(goalText);
+        }
+
+        private void OnMouseEnter()
+        {
+            UIManager.Instance.ShowHover(interactionName);
+        }
+
+        private void OnMouseExit()
+        {
+            UIManager.Instance.HideHover();
+        }
+
+        private void OnMouseDown()
+        {
+            ACon.Instance.SetTargetInteraction(this);
+        }
+        #endregion
+
+        #region Interaction
+        public void ExecuteInteraction()
+        {
+            UIManager.Instance.ShowDialogue(dialogueLines, FinishInteraction);
+        }
+
+        private void FinishInteraction()
+        {
+            if (nextInteraction != null)
+            {
+                nextInteraction.SetActive(true);
+            }
+            else
+            {
+                UIManager.Instance.ShowDayEnd();
+            }
+
+            gameObject.SetActive(false);
+        }
+        #endregion
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            if (interactionPoint == null) return;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(interactionPoint.position, interactionRadius);
+        }
+#endif
     }
 }
