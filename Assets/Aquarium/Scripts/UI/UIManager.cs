b924f7b3bcb84ee491bd04c984dee216ae2deaa0
DESKTOP-5V3FRMH\M <mulddung2000@gmail.com>
2025-12-30 18:18:25 +0900

    1. 스크립트 정리

--- Assets/Aquarium/Scripts/UI/UIManager.cs
+++ Assets/Aquarium/Scripts/UI/UIManager.cs
@@ -1,140 +1,3 @@
-/*using UnityEngine;
-using TMPro;
-using System;
-
-namespace Aquarium
-{
-    public class UIManager : MonoBehaviour
-    {
-        #region Singleton
-        public static UIManager Instance;
-        #endregion
-
-        #region Variables
-        [Header("Goal UI")]
-        [SerializeField] private GameObject goalPanel;
-        [SerializeField] private TextMeshProUGUI goalText;
-
-        [Header("Hover UI")]
-        [SerializeField] private GameObject hoverPanel;
-        [SerializeField] private TextMeshProUGUI hoverText;
-
-        [Header("Dialogue UI")]
-        [SerializeField] private GameObject dialoguePanel;
-        [SerializeField] private TextMeshProUGUI dialogueText;
-
-        [Header("Day End UI")]
-        [SerializeField] private GameObject dayEndPanel;
-
-        private bool isDialogueActive = false;
-
-        // Dialogue control
-        private string[] currentLines;
-        private int currentIndex;
-        private Action onDialogueFinished;
-        #endregion
-
-        #region Unity Event Methods
-        private void Awake()
-        {
-            if (Instance == null)
-                Instance = this;
-            else
-                Destroy(gameObject);
-        }
-
-        private void Update()
-        {
-            if (!isDialogueActive)
-                return;
-
-            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-            {
-                AdvanceDialogue();
-            }
-        }
-        #endregion
-
-        #region Hover UI
-        public void ShowHover(string text)
-        {
-            if (isDialogueActive)
-                return;
-
-            hoverText.text = text;
-            hoverPanel.SetActive(true);
-        }
-
-        public void HideHover()
-        {
-            hoverPanel.SetActive(false);
-        }
-        #endregion
-
-        #region Goal UI
-        public void SetGoal(string text)
-        {
-            goalText.text = text;
-            goalPanel.SetActive(true);
-        }
-
-        public void HideGoal()
-        {
-            goalPanel.SetActive(false);
-        }
-        #endregion
-
-        #region Dialogue UI
-        public void ShowDialogue(string[] lines, Action onFinished)
-        {
-            if (lines == null || lines.Length == 0)
-                return;
-
-            currentLines = lines;
-            currentIndex = 0;
-            onDialogueFinished = onFinished;
-
-            isDialogueActive = true;
-
-            HideHover();
-            HideGoal();
-
-            dialoguePanel.SetActive(true);
-            dialogueText.text = currentLines[currentIndex];
-        }
-
-        private void AdvanceDialogue()
-        {
-            currentIndex++;
-
-            if (currentIndex < currentLines.Length)
-            {
-                dialogueText.text = currentLines[currentIndex];
-            }
-            else
-            {
-                HideDialogue();
-                onDialogueFinished?.Invoke();
-            }
-        }
-
-        public void HideDialogue()
-        {
-            isDialogueActive = false;
-            dialoguePanel.SetActive(false);
-        }
-        #endregion
-
-        #region Day End
-        public void ShowDayEnd()
-        {
-            HideHover();
-            SetGoal("End Week");
-            dayEndPanel.SetActive(true);
-        }
-        #endregion
-    }
-}*/
 using UnityEngine;
 using TMPro;
 using System;
@@ -199,25 +62,7 @@ namespace Aquarium
         #endregion
 
         #region State
-        /*public void SetState(UIState newState)
-        {
-            currentState = newState;
-
-            // 공통 정리
-            hoverPanel.SetActive(false);
-            dialoguePanel.SetActive(false);
-            dayEndPanel.SetActive(false);
-
-            //  핵심: Dialogue 중 Goal 숨김
-            if (newState == UIState.Dialogue)
-            {
-                goalPanel.SetActive(false);
-            }
-            else
-            {
-                goalPanel.SetActive(true);
-            }
-        }*/
+        
         public void SetState(UIState newState)
         {
             currentState = newState;
@@ -311,16 +156,6 @@ namespace Aquarium
                 EndDialogue();
             }
         }
-
-        /*private void EndDialogue()
-        {
-            dialoguePanel.SetActive(false);
-            currentDialogueLines = null;
-            onDialogueEnd?.Invoke();
-            onDialogueEnd = null;
-
-            SetState(UIState.None);
-        }*/
         private void EndDialogue()
         {
             dialoguePanel.SetActive(false);
